c728806b5f9c5dc3a894cd90a73bddbd363d7baa
LevitatingBusinessMan (Rein Fernhout) <me@levitati.ng>
2023-11-29 00:18:35 +0100

    color for new stuff and failure messages

--- app/jobs/download_feed_job.rb
+++ app/jobs/download_feed_job.rb
@@ -3,12 +3,22 @@ class DownloadFeedJob < ApplicationJob
   queue_as :default
 
   def perform sub
-    sub.get_rss
-    SubscriptionsChannel.broadcast_replace_to(
-      :subscriptions,
-      target: dom_id(sub),
-      partial: 'feed/subscription',
-      locals: { sub: sub }
-    )
+    begin
+      sub.get_rss
+      SubscriptionsChannel.broadcast_replace_to(
+        :subscriptions,
+        target: dom_id(sub),
+        partial: 'feed/subscription',
+        locals: { sub: sub }
+      )
+    rescue OpenURI::HTTPError, Errno::ENOENT => ex
+      logger.error "#{sub.name.inspect} failed to download from #{sub.url}: #{ex.inspect}"
+      SubscriptionsChannel.broadcast_replace_to(
+        :subscriptions,
+        target: dom_id(sub),
+        partial: 'feed/subscription_failed',
+        locals: { sub: sub, exception: ex }
+      )
+    end
   end
 end
